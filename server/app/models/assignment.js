88515cc7e5338405130293d4aba0f54f380defa0
FMurry <frederic.wesley.murry@gmail.com>
2016-12-28 19:45:17 -0800

    Added assignment.js

---
+++ server/app/models/assignment.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Look at course.js for reference
+//Create a schema for assignments
+
+
+//Code Here
+
+
+
+
+
+
+
+
+
+//Uncomment this line when finished
+//module.exports = mongoose.model('Assignment', assignmentSchema);
