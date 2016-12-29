00a265762d87715105c67a3db38230950328b687
FMurry <frederic.wesley.murry@gmail.com>
2016-12-28 19:49:36 -0800

    Added instructions to assignment file

--- server/app/models/assignment.js
+++ server/app/models/assignment.js
@@ -4,6 +4,11 @@ var Schema = mongoose.Schema;
 //Look at course.js for reference
 //Create a schema for assignments
 
+/* Each assigment has a reference to one Course
+For Example one field in Schema will be
+course_ID: {type: mongoose.Schema.Types.ObjectId,  ref: 'Course'},
+*/
+
 
 //Code Here
 
